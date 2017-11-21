aa5aaf04e877eac8cd38e8f699d2f4fdfffb62fc
Tal Wrii <talwrii@gmail.com>
2017-11-22 00:22:41 +0100

    tag version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ HERE = os.path.dirname(__file__)
 
 setuptools.setup(
     name='inotify_httpd',
-    version="0.1.0",
+    version="0.1.1",
     author='Tal Wrii',
     author_email='talwrii@gmail.com',
     description='',
